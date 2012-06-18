945ab9b070c154f3fe02cb49f7e0b272d5dc12a2
Darren Ethier <darren@roughsmootheng.in>
2012-06-18 11:08:09 -0400

    [completed: 15]

--- organize-series-extra-tokens.php
+++ organize-series-extra-tokens.php
@@ -8,7 +8,7 @@ Author URI: http://organizeseries.com
 */
 
 /* LICENSE */
-//"Organize Series Plugin" and all addons for it created by this author are copyright (c) 2007-2011 Darren Ethier. This program is free software; you can redistribute it and/or
+//"Organize Series Plugin" and all addons for it created by this author are copyright (c) 2007-2012 Darren Ethier. This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
 // as published by the Free Software Foundation; either version 2
 // of the License, or (at your option) any later version.
