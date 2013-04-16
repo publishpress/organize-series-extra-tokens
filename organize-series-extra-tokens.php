7044ac1e01185800dfbfdaed6b87672557af4d5e
Darren Ethier <darren@roughsmootheng.in>
2013-04-16 11:22:18 -0400

    minor version bump to account for users who downloaded the wrong file from orgseries.com b/c I actually didn't update the file there.

--- organize-series-extra-tokens.php
+++ organize-series-extra-tokens.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Organize Series Addon - Extra Tokens
 Description: This addon for the Organize Series plugin provides Extra %tokens% for customizing the auto-inserted output of series related information.
-Version: 0.8
+Version: 0.8.1
 Author: Darren Ethier
 Author URI: http://organizeseries.com
 */
